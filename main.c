d0430fb4df8ee49371ac1f251277707b0e501ccf
jbillaba <jeremyvillalva66@gmail.com>
2023-11-20 14:27:02 -0500

    made a menu working on new file function

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 # include <stdio.h>
+void system();
 
-void doMath(int x, int y) {
-	x = x + y;
-	printf("%d",x);
+char switchMenu() {
+	char menuSelection;
+
+	printf("enter one of the following chars n, e\n n makes a new file and e exits the program\n");
+	scanf("%c", &menuSelection);
+
+	switch (menuSelection) {
+		case 'n':
+			printf("this should make a new file");
+			break;
+
+		case 'e': 
+			printf("exit the program");
+			break;
+		default: 
+			printf("print a help message for user");
+			break;
+
+	}
+
+	return menuSelection ; 
 }
 
+
 int main() {
-	doMath(3,2);
+	system("clear");
+	// run function here
+	switchMenu();
 	return 0;
 }
 
